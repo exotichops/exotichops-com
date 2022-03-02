0552685b745fef6bff8734c243632efacc1ee455
exotichops <100741056+exotichops@users.noreply.github.com>
2022-03-01 18:12:47 -0800

    Updating index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Exotic Hops, Inc.</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -14,6 +14,9 @@ export default function Home() {
         <Header title="ExoticHops.com" />
         <p className="description">
           Providing and preserving <code>unique, exotic, and flavorful hops</code> for generations to come.
+        </p>
+        <p className="description">
+          Contact us at <a href="mailto:exotichopsco@gmail.com">exotichopsco@gmail.com</a> for more information.
         </p>
       </main>
 
