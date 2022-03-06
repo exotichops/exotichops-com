9fe302f0963a08870781ae659120234a72988eac
exotichops <100741056+exotichops@users.noreply.github.com>
2022-03-05 18:47:32 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,15 +20,15 @@ export default function Home() {
           Questions/Comments? Contact us below: 
         </p>
     
-        <form name="contact" netlify>
+        <form name="contact" method="POST" data-netlify="true">
           <p>
-            <label>Name <input type="text" name="name" /></label>
+            <label>Your Name: <input type="text" name="name" /></label>   
           </p>
           <p>
-            <label>Email <input type="email" name="email" /></label>
-          </p>
+            <label>Your Email: <input type="email" name="email" /></label>
+          </p>          
           <p>
-            <label>Message <textarea rows="2" name="message"/></label>
+            <label>Message: <textarea name="message"></textarea></label>
           </p>
           <p>
             <button type="submit">Send</button>
