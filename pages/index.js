ab186717838a90612d0b3ee25aef1888172b4be8
exotichops <100741056+exotichops@users.noreply.github.com>
2022-03-05 18:37:34 -0800

    Adding contact form

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+fimport Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
@@ -13,8 +13,23 @@ export default function Home() {
       <main>
         <img src="https://raw.githubusercontent.com/exotichops/exotichops-com/main/public/logo.svg" width="319" height="353" alt="ExoticHops.com" />
        
-        <p className="description">
-          Contact <a href="mailto:exotichopsco@gmail.com">exotichopsco@gmail.com</a> for information.
+        <p className="description">         
+          Questions/Comments? Contact us below: 
+    
+          <form name="contact" netlify>
+          <p>
+            <label>Name <input type="text" name="name" /></label>
+          </p>
+          <p>
+            <label>Email <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>Message <textarea rows="2" name="message"/></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
         </p>
       </main>
       <Footer />
